c0a9d4572baf035a31880c04435df373e7cd9c18
Brian Shockency <53105161+Bshock817@users.noreply.github.com>
2020-03-29 09:10:04 -0400

    Add files via upload

---
+++ Recursion/todo1.js
@@ -0,0 +1,24 @@
+// Recursive Sigma
+function recursiveSigma(num){
+    if(num == 0){
+        return 0;
+    }
+    if(num > 0){
+        return num + recursiveSigma(num-1);
+    }
+}
+
+console.log(recursiveSigma(5));
+
+//recursive Factorial
+
+function recursiveFactorial(num){
+    if(num <= 0){
+        return 1;
+    }
+    if(num > 0){
+        return num * recursiveFactorial(num-1);
+    }
+}
+
+console.log(recursiveFactorial(5));
